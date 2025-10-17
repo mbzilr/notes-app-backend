7f9c251f4b50dd1b286881ab346c435aa0db0738
Malik Bazil Rabbani <asterion911ts@gmail.com>
2025-10-17 10:28:05 +0700

    Initial Commit.

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import daStyle from 'eslint-config-dicodingacademy';
+
+export default defineConfig([
+  daStyle,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      'linebreak-style': 'off',
+    },
+  },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+]);
